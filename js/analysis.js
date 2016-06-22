0ef79684b1e914c7d5e9bdfb934ccf284b2bda39
Steven Weng <Steven_Weng@epam.com>
2016-06-22 17:53:40 +0800

    implement di-da ball loading

--- js/analysis.js
+++ js/analysis.js
@@ -1,14 +1,21 @@
-(function(i, s, o, g, r, a, m) {
-	i['GoogleAnalyticsObject'] = r;
-	i[r] = i[r] || function() {
-		(i[r].q = i[r].q || []).push(arguments)
-	}, i[r].l = 1 * new Date();
-	a = s.createElement(o),
-	m = s.getElementsByTagName(o)[0];
-	a.async = 1;
-	a.src = g;
-	m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-53028337-3', 'auto');
-ga('send', 'pageview');
+// Ignore analysis at local
+if(location.href.indexOf('file://') !== 0){
+	/* google analysis */
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-53028337-1', 'auto');
+	  ga('send', 'pageview');
+
+
+	 /* baidu analysis */
+	 /*
+	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+	document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F3fe1382df650c13fdd070ed31859df9c' type='text/javascript'%3E%3C/script%3E"));
+
+	*/
+
+}
